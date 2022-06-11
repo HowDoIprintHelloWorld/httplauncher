0ee82a5849a7af6277f4add17295c5b7d9f639da
HowDoIprintHelloWorld <87938212+HowDoIprintHelloWorld@users.noreply.github.com>
2022-06-11 09:43:10 +0200

    Added framework functionality

--- httplauncher.go
+++ httplauncher.go
@@ -6,11 +6,17 @@ import (
   "flag"
   "os"
   "net"
+  "io/ioutil"
+  "strings"
+  "net/http"
+  "bufio"
 )
 
 func main() {
   verbool, urlstring, wordlstring, timefloat, concurrint, refstring, datastring := parseargs()
   fmt.Println(verbool, urlstring, wordlstring, timefloat, concurrint, refstring, datastring)
+  res :=  request(urlstring, wordlstring, datastring)
+  fmt.Println(res)
 }
 
 func validate(urlstring string, datastring string, wordlstring string) {
@@ -35,9 +41,38 @@ func parseargs() (bool, string, string, float64, int, string, string) {
   flag.Parse()
 
   if (*wordlflag == "" || *refflag == "" || *dataflag == "") {
+    fmt.Println(*wordlflag, *refflag, *dataflag)
     fmt.Println("Not enough info!")
     os.Exit(1)
   }
 
   return *verbflag, *urlflag, *wordlflag, *timeflag, *concurrflag, *refflag, *dataflag
+}
+
+func getdatastring(datastring string) string {
+  b, err := ioutil.ReadFile(datastring)
+    if err != nil {
+        fmt.Print(err)
+    }
+    return string(b)
+}
+
+func request(urlstring string, wordlstring string, datastring string) string {
+  data := getdatastring(datastring)
+  readFile, err := os.Open(wordlstring)
+  if err != nil {
+          os.Exit(1)
+  }
+  fileScanner := bufio.NewScanner(readFile)
+  fileScanner.Split(bufio.ScanLines)
+  for fileScanner.Scan() {
+    newdata := strings.Replace(data, "^P1^", fileScanner.Text(), -1)
+    _, err := http.NewRequest("POST", urlstring, strings.NewReader(newdata))
+    if err != nil {
+      os.Exit(1)
+    }
+  }
+
+  readFile.Close()
+  return "tmp"
 }
